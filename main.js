e1ed79e87ff377a36cf00cc08c1879e28ebc5d8a
Longs Pemun Gotar <developerlongs@gmail.com>
2022-09-04 11:32:43 +0000

    added method to check blockchain validity

--- main.js
+++ main.js
@@ -36,10 +36,29 @@ class Blockchain {
     newBlock.hash = newBlock.calculateHash();
     this.chain.push(newBlock);
   }
+
+  isChainValid(){
+    for(let i = 1; i < this.chain.length; i++){
+        const currentBlock = this.chain[i];
+        const previousBlock = this.chain[i -1];
+
+        if(currentBlock.hash !== currentBlock.calculateHash()){
+            return false;
+        }
+
+        if(currentBlock.previousHash !== previousBlock.hash){
+            return false;
+        }
+    }
+
+    return true;
+  }
 }
 
 let longsCoin = new Blockchain();
 longsCoin.addBlock(new Block(1, "03/03/2022", {amount: 4}));
 longsCoin.addBlock(new Block(2, "04/12/2019", {amount: 50}));
 
-console.log(JSON.stringify(longsCoin, null, 4));
+//console.log(JSON.stringify(longsCoin, null, 4));
+
+console.log("Is blockchain valid?", longsCoin.isChainValid())
